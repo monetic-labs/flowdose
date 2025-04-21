46a3b67dba9a5701778684a69b3dde5b5b3f4dc4
notdevin <devinjelliot@gmail.com>
2025-04-21 08:28:13 -0700

    terraform issues

--- terraform/setup-state-bucket.tf
+++ terraform/setup-state-bucket.tf
@@ -2,20 +2,21 @@
 # After running this once, you can comment out the resource and 
 # uncomment the backend configuration in backend.tf
 
-resource "digitalocean_spaces_bucket" "terraform_state" {
-  name   = "flowdose-terraform-state"
-  region = var.spaces_region
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    enabled = true
-
-    noncurrent_version_expiration {
-      days = 90
-    }
-  }
-} 
+# Resource commented out because the bucket already exists
+# resource "digitalocean_spaces_bucket" "terraform_state" {
+#   name   = "flowdose-terraform-state"
+#   region = var.spaces_region
+#   acl    = "private"
+#
+#   versioning {
+#     enabled = true
+#   }
+#
+#   lifecycle_rule {
+#     enabled = true
+#
+#     noncurrent_version_expiration {
+#       days = 90
+#     }
+#   }
+# } 
