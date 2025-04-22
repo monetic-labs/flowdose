e3078841d4ff0738018231b795552f8d7ca2fad1
notdevin <devinjelliot@gmail.com>
2025-04-22 15:01:11 -0700

    yep

--- terraform/modules/dns/outputs.tf
+++ terraform/modules/dns/outputs.tf
@@ -1,19 +1,19 @@
 output "domain_name" {
   description = "The domain name"
-  value       = digitalocean_domain.flowdose.name
+  value       = data.digitalocean_domain.flowdose.name
 }
 
 output "api_fqdn" {
   description = "Fully qualified domain name for the API"
-  value       = "${digitalocean_record.backend_api.name}.${digitalocean_domain.flowdose.name}"
+  value       = "${digitalocean_record.backend_api.name}.${data.digitalocean_domain.flowdose.name}"
 }
 
 output "admin_fqdn" {
   description = "Fully qualified domain name for the admin panel"
-  value       = "${digitalocean_record.backend_admin.name}.${digitalocean_domain.flowdose.name}"
+  value       = "${digitalocean_record.backend_admin.name}.${data.digitalocean_domain.flowdose.name}"
 }
 
 output "storefront_fqdn" {
   description = "Fully qualified domain name for the storefront"
-  value       = digitalocean_record.storefront.name == "@" ? digitalocean_domain.flowdose.name : "${digitalocean_record.storefront.name}.${digitalocean_domain.flowdose.name}"
+  value       = digitalocean_record.storefront.name == "@" ? data.digitalocean_domain.flowdose.name : "${digitalocean_record.storefront.name}.${data.digitalocean_domain.flowdose.name}"
 } 
